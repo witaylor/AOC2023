2384b62397da9a8ecefac2ff8505dd7f8b8b2dcd
Will Taylor <witaylor@expediagroup.com>
2023-12-01 14:05:49 +0000

    create day two package

---
+++ src/two/Day02.kt
@@ -0,0 +1,12 @@
+package two
+
+class Day02 {
+
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+}
